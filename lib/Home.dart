f13baee54bb0bda78a45ab4f6659e3a4173fa5a3
Sandro Ferreira <sandrorf1973@gmail.com>
2023-04-09 19:39:32 -0300

    Aula - 197

--- lib/Home.dart
+++ lib/Home.dart
@@ -13,6 +13,8 @@ class _HomeState extends State<Home> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
+  Set<Marker> _marcadores = {};
+
   _onMapCreated(GoogleMapController googleMapController) {
     _controller.complete(googleMapController);
   }
@@ -25,6 +27,43 @@ class _HomeState extends State<Home> {
             zoom: 16,
             tilt: 0,
             bearing: 30)));
+  }
+
+  _carregarMarcadores() {
+    Set<Marker> marcadoresLocal = {};
+
+    Marker marcadorShopping = Marker(
+        markerId: const MarkerId("marcador-shopping"),
+        position: const LatLng(-23.56331643290178, -46.652642650333135),
+        infoWindow: const InfoWindow(title: "Shopping Cidade São Paulo"),
+        icon:
+            BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueMagenta),
+        onTap: () {
+          // ignore: avoid_print
+          print("shopping clicado!");
+        }
+        //rotation: 45
+        );
+
+    Marker marcadorCartorio = Marker(
+        markerId: const MarkerId("marcador-cartorio"),
+        position: const LatLng(-23.56483325943213, -46.65435843245621),
+        infoWindow: const InfoWindow(title: "12° Cartório de Notas"),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue));
+
+    marcadoresLocal.add(marcadorShopping);
+    marcadoresLocal.add(marcadorCartorio);
+
+    setState(() {
+      _marcadores = marcadoresLocal;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _carregarMarcadores();
   }
 
   @override
@@ -44,8 +83,9 @@ class _HomeState extends State<Home> {
         // mapType: MapType.normal,
         mapType: MapType.normal,
         initialCameraPosition: const CameraPosition(
-            target: LatLng(-23.6631361, -46.540326), zoom: 16),
+            target: LatLng(-23.56331643290178, -46.652642650333135), zoom: 16),
         onMapCreated: _onMapCreated,
+        markers: _marcadores,
       ),
     );
   }
