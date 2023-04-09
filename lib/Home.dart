b1858acf7010b584a5b7e4abc31ff3a2bf9edfc5
Sandro Ferreira <sandrorf1973@gmail.com>
2023-04-09 10:07:24 -0300

    Aula - 196

--- lib/Home.dart
+++ lib/Home.dart
@@ -17,11 +17,27 @@ class _HomeState extends State<Home> {
     _controller.complete(googleMapController);
   }
 
+  _movimentarCamera() async {
+    GoogleMapController googleMapController = await _controller.future;
+    googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+        const CameraPosition(
+            target: LatLng(-23.6631361, -46.540326),
+            zoom: 16,
+            tilt: 0,
+            bearing: 30)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Mapas e geolocalização"),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      floatingActionButton: FloatingActionButton(
+        // ignore: sort_child_properties_last
+        child: const Icon(Icons.done),
+        onPressed: _movimentarCamera,
       ),
       // ignore: avoid_unnecessary_containers
       body: GoogleMap(
