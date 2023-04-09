f1f97639a5e81f15bc86c82496c9744017062057
Sandro Ferreira <sandrorf1973@gmail.com>
2023-04-09 09:40:29 -0300

    Aula - 195

--- lib/Home.dart
+++ lib/Home.dart
@@ -13,6 +13,10 @@ class _HomeState extends State<Home> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
+  _onMapCreated(GoogleMapController googleMapController) {
+    _controller.complete(googleMapController);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,12 +25,11 @@ class _HomeState extends State<Home> {
       ),
       // ignore: avoid_unnecessary_containers
       body: GoogleMap(
+        // mapType: MapType.normal,
         mapType: MapType.normal,
         initialCameraPosition: const CameraPosition(
             target: LatLng(-23.6631361, -46.540326), zoom: 16),
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+        onMapCreated: _onMapCreated,
       ),
     );
   }
